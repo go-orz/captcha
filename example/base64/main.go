a88b3ed8f6f9e947d953a5874b9d41c134dba274
dushixiang <dushixiang@zhuozhuo.io>
2023-10-28 20:08:55 +0800

    change org

--- example/base64/main.go
+++ example/base64/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 
-	"github.com/dushixiang/captcha"
+	"github.com/go-orz/captcha"
 )
 
 func main() {
--- example/file/main.go
+++ example/file/main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/dushixiang/captcha"
+	"github.com/go-orz/captcha"
 )
 
 func main() {
